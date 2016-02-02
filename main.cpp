45497bca814a9326eaa9bf6579f9dee6f259790b
KUANG Qi <kqwd@163.com>
2016-02-02 14:31:39 +0800

    Immediate Alert Service as a class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "ble/BLE.h"
 #include "ble/services/HealthThermometerService.h"
+#include "ble/services/ImmediateAlertService.h"
 #include "ADT7420.h"
 
 //BLE ble;
@@ -12,26 +13,17 @@ Ticker measureTimer;
 DigitalOut greenLED(p21, 0);
 DigitalOut redLED(p20, 0);
 
-
 static bool shouldMeasure = true;
 static const char *DEVICE_NAME = "BNU_Biosen";
-static const uint16_t uuid16_list[] = {0x1802, GattService::UUID_HEALTH_THERMOMETER_SERVICE};
-
-uint8_t alertLevel;
-GattCharacteristic alert(GattCharacteristic::UUID_ALERT_LEVEL_CHAR,
-		&alertLevel,
-		1, 1,
-		GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE_WITHOUT_RESPONSE);
-GattCharacteristic *characteristics[] = {&alert};
-GattService immediateAlertService(0x1802, characteristics,
-		sizeof(characteristics) / sizeof(GattCharacteristic *));
+static const uint16_t uuid16_list[] = {GattService::UUID_IMMEDIATE_ALERT_SERVICE,
+                                       GattService::UUID_HEALTH_THERMOMETER_SERVICE};
 
 HealthThermometerService *thermo;
+ImmediateAlertService *alert;
 
 void tickAlertHandler()
 {
 	redLED = !redLED;
-	//greenLED = !greenLED;
 }
 
 void tickMeasureHandler()
@@ -39,10 +31,29 @@ void tickMeasureHandler()
 	shouldMeasure = true;
 }
 
+void alertHandler(ImmediateAlertService::AlertLevel_t alertLevel)
+{
+    if(alertLevel == ImmediateAlertService::NO_ALERT)
+    {
+        alertTimer.detach();
+        redLED = 0;
+    }
+    if(alertLevel == ImmediateAlertService::MILD_ALERT)
+    {
+        alertTimer.attach(tickAlertHandler, 0.5);
+    }
+    if(alertLevel == ImmediateAlertService::HIGH_ALERT)
+    {
+        alertTimer.attach(tickAlertHandler, 0.25);
+    }
+}
+
 void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *)
 {
     BLE::Instance(BLE::DEFAULT_INSTANCE).gap().startAdvertising();
     greenLED = 0;
+    alert->setAlertLevel(ImmediateAlertService::NO_ALERT);
+    alertHandler(ImmediateAlertService::NO_ALERT);
 }
 
 void connectionCallback(const Gap::ConnectionCallbackParams_t *)
@@ -52,31 +63,7 @@ void connectionCallback(const Gap::ConnectionCallbackParams_t *)
 
 void writeCharCallback(const GattWriteCallbackParams *params)
 {
-	GattAttribute::Handle_t ah = alert.getValueHandle();
-    /* Check to see what characteristic was written, by handle */
-    if(params->handle == ah) {
-        /* toggle LED if only 1 byte is written */
-        if(params->len == 1) {
-            if(params->data[0] == 1)
-            	alertTimer.attach(tickAlertHandler, 0.5);
-            if(params->data[0] == 2)
-            	alertTimer.attach(tickAlertHandler, 0.1);
-            if(params->data[0] == 0)
-            	alertTimer.detach();
-            //(params->data[0] == 0x00) ? printf("led on\n\r") : printf("led off\n\r"); // print led toggle
-        }
-        /* Print the data if more than 1 byte is written */
 
-            printf("Data received: length = %d, data = 0x",params->len);
-            for(int x=0; x < params->len; x++) {
-                printf("%x", params->data[x]);
-            }
-            printf("\n\r");
-
-
-        /* Update the readChar with the value of writeChar */
-        //BLE::Instance(BLE::DEFAULT_INSTANCE).gattServer().write(readChar.getValueHandle(), params->data, params->len);
-    }
 }
 
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
@@ -90,7 +77,7 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
 
     ble.gap().onDisconnection(disconnectionCallback);
     ble.gap().onConnection(connectionCallback);
-    ble.gattServer().onDataWritten(writeCharCallback);
+    //ble.gattServer().onDataWritten(writeCharCallback);
 
     /* Setup advertising */
     ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE); // BLE only, no classic BT
@@ -105,8 +92,8 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
     measureTimer.attach(tickMeasureHandler, 1.0);
 
     /* Add our custom service */
-    ble.addService(immediateAlertService);
     thermo = new HealthThermometerService(ble, (float)tsensor, HealthThermometerService::LOCATION_BODY);
+    alert  = new ImmediateAlertService(ble, alertHandler);
 
     /* Start advertising */
     ble.gap().startAdvertising();
