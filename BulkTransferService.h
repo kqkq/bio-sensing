6c27c68559976ed6a4f1eaa519e7f5af1d96aac8
Kuang Qi <kqwd@163.com>
2016-03-02 15:59:36 +0800

    Bulk Transfer Service refactored

--- BulkTransferService.h
+++ BulkTransferService.h
@@ -11,7 +11,10 @@
 #include "ble/BLE.h"
 
 typedef void (*RequestCallback)(int addr, int len);
+
 #define PAYLOAD_SIZE 16
+#define UUID_BULK_DATA          0x1111
+#define UUID_BULK_CONTROL_POINT 0x2222
 
 class BulkTransferService
 {
@@ -22,151 +25,151 @@ public:
      * @param[ref] ble
      *               BLE object for the underlying controller.
      */
-    BulkTransferService(BLE &bleIn, uint8_t *dataBuffer, int bufferSize, RequestCallback callback) :
+    BulkTransferService(BLE &bleIn, uint8_t *dataBuffer, int bufSize, RequestCallback callback) :
             ble(bleIn),
-            dataptr(dataBuffer),
-			cursor(dataBuffer),
-            bulkTransferData(0x1111, cursor, PAYLOAD_SIZE, PAYLOAD_SIZE, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY),
-            bulkTransferControlPoint(0x2222, (uint8_t *)config, 8, 8, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE)
+            bufferBase(dataBuffer),
+			bufferCursor(dataBuffer),
+            bulkTransferData(UUID_BULK_DATA, bufferCursor, PAYLOAD_SIZE, PAYLOAD_SIZE, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY),
+            bulkTransferControlPoint(UUID_BULK_CONTROL_POINT, (uint8_t *)controlPointBase, 8, 8, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ | GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_WRITE)
     {
         static bool serviceAdded = false; /* We should only ever add one Immediate Alert service. */
-        bufSize = bufferSize;
+        bufferSize = bufSize;
         requestData = callback;
-        reqLength = 0;
-        reqAddr = 0;
-        remainBuf = 0;
-        remainTrans = 0;
-        transAddr = 0;
-        dataSubscibed = false;
+        lengthRequested = 0;
+        addressRequested = 0;
+        bytesInBuffer = 0;
+        bytesInTransaction = 0;
+        addressCursor = 0;
+        isSubscibed = false;
 
         if (serviceAdded)
         {
             return;
         }
 
-        GattCharacteristic *charTable[] =
-        { &bulkTransferData, &bulkTransferControlPoint };
-        GattService bulkTransferService(0x1825, charTable,
-                sizeof(charTable) / sizeof(GattCharacteristic *));
+        GattCharacteristic *charTable[] = {&bulkTransferData, &bulkTransferControlPoint};
+        GattService bulkTransferService(0x1825, charTable, sizeof(charTable) / sizeof(GattCharacteristic *));
         ble.gattServer().addService(bulkTransferService);
         serviceAdded = true;
 
         ble.gattServer().onDataWritten(this, &BulkTransferService::onDataWritten);
+        ble.gattServer().onDataSent(this, &BulkTransferService::onDataSent);
         ble.gattServer().onUpdatesEnabled(FunctionPointerWithContext<uint16_t>(this, &BulkTransferService::onUpdatesEnabled));
         ble.gattServer().onUpdatesDisabled(FunctionPointerWithContext<uint16_t>(this, &BulkTransferService::onUpdatesDisables));
     }
 
     uint32_t getAddress()
     {
-    	return config[0];
+    	return addressRequested;
     }
 
     uint32_t getLength()
     {
-    	return config[1];
+    	return lengthRequested;
     }
 
     void dataPrepared(int length)
     {
-    	remainBuf = length;
-    	if(dataSubscibed)
+    	if(isSubscibed)
     	{
-    		cursor = dataptr;
-    		sendAPacket();
+    		bytesInBuffer = length;
+    		bufferCursor = bufferBase;
+    		sendBuffer();
     	}
     }
 
 protected:
     /**
-     * This callback allows receiving updates to the AlertLevel characteristic.
+     * This callback allows receiving updates to the Bulk Transfer Control Point characteristic.
      *
      * @param[in] params
      *     Information about the characteristic being updated.
      */
     virtual void onDataWritten(const GattWriteCallbackParams *params)
     {
-    	int hdl = bulkTransferControlPoint.getValueHandle();
-        if (params->handle == hdl)
+        if (params->handle == bulkTransferControlPoint.getValueHandle())
         {
-        	uint8_t *ptr = (uint8_t *)config;
+        	uint8_t *ptr = (uint8_t *)controlPointBase;
         	for(int i = 0; i < 8; i++) ptr[i] = params->data[i];
-        	reqAddr = config[0];
-        	reqLength = config[1];
-        	transAddr = reqAddr;
-        	remainTrans = reqLength;
-            if(remainTrans > 0)
+        	addressRequested = controlPointBase[0];
+        	lengthRequested = controlPointBase[1];
+        	addressCursor = addressRequested;
+        	bytesInTransaction = lengthRequested;
+            if(bytesInTransaction > 0)
             {
-            	requestData(transAddr, remainTrans < bufSize ? remainTrans : bufSize);
+            	printf("%d bytes of data from 0x%06x are requested\r\n", lengthRequested, addressRequested);
+            	requestData(addressCursor, bytesInTransaction < bufferSize ? bytesInTransaction : bufferSize);
             }
         }
     }
 
+    void onDataSent(unsigned count)
+    {
+    	sendBuffer();
+    }
+
     virtual void onUpdatesEnabled(uint16_t handle)
     {
-    	if(handle == bulkTransferData.getValueHandle())
-    	{
-    		dataSubscibed = true;
-    		printf("Subscribed!\r\n");
-    	}
+    	if(handle == bulkTransferData.getValueHandle()) isSubscibed = true;
     }
 
     virtual void onUpdatesDisables(uint16_t handle)
     {
-    	if(handle == bulkTransferData.getValueHandle())
-    	{
-    		dataSubscibed = false;
-    		printf("Unsubscribed!\r\n");
-    	}
+    	if(handle == bulkTransferData.getValueHandle()) isSubscibed = false;
     }
 
-    virtual int sendAPacket()
+    virtual void sendBuffer()
     {
-    	int len = remainBuf < PAYLOAD_SIZE ? remainBuf : PAYLOAD_SIZE;
-
-    	while(remainBuf > 0)
+    	int len;
+    	ble_error_t didSendValue = BLE_ERROR_NONE;
+    	while(bytesInBuffer > 0)
     	{
-			ble.gattServer().write(bulkTransferData.getValueHandle(), cursor, len);
-			cursor += len;
-			remainBuf -= len;
-			remainTrans -= len;
-			transAddr += len;
-
-			//printf("%d sent, rbuf=%d, rtrans=%d!\r\n", len, remainBuf, remainTrans);
+    		len = bytesInBuffer < PAYLOAD_SIZE ? bytesInBuffer : PAYLOAD_SIZE;
+			didSendValue = ble.gattServer().write(bulkTransferData.getValueHandle(), bufferCursor, len);
+			if(didSendValue != BLE_ERROR_NONE) break;
+			//for(int i = 0; i < len; i++) printf("%x", (int)bufferCursor[i]);
+			//printf("\r\n");
+			bufferCursor += len;
+			bytesInBuffer -= len;
+			bytesInTransaction -= len;
+			addressCursor += len;
     	}
-    	if(remainTrans != 0)
+    	if(bytesInTransaction != 0 && bytesInBuffer == 0)
 		{
-			requestData(transAddr, remainTrans < bufSize ? remainTrans : bufSize);
+			requestData(addressCursor, bytesInTransaction < bufferSize ? bytesInTransaction : bufferSize);
 		}
-    	return len;
     }
 
 protected:
     BLE &ble;
 
     // Pointer to the data buffer
-    uint8_t *dataptr;
+    uint8_t *bufferBase;
     // Cursor to the data buffer
-    uint8_t *cursor;
+    uint8_t *bufferCursor;
     // Length of the data buffer
-    int bufSize;
+    int      bufferSize;
+    // Memory of Bulk Transfer Control Point
+    uint32_t controlPointBase[2]; // [0]: address  [1]: length
 
-    // Address of data(in flash) which are requested by the client
-    int reqAddr;
-    // Length of data(in flash) which are requested by the client
-    int reqLength;
-    //
-    int transAddr;
-    // Length of data(in buffer) which will be transmitted in a transaction
-    volatile int remainBuf;
-    // Length of data which will be transmitted in a request
-    int remainTrans;
+    // Address of data (in flash) which are requested by the client
+    int addressRequested;
+    // A cursor of data (in flash)
+    int addressCursor;
+    // Length of data (in flash) which are requested by the client
+    int lengthRequested;
 
-    bool dataSubscibed;
+    // Number of bytes to be sent in the buffer
+    int bytesInBuffer;
+    // Number of bytes to be sent in a requested transaction
+    int bytesInTransaction;
+    // Characteristic Bulk Transfer Data is subscribed by client
+    bool isSubscibed;
 
-    // BLE characteristic storage
-    uint32_t config[2]; //config[0]: address  config[1]: length
+    // BLE characteristics
     GattCharacteristic bulkTransferData;
     GattCharacteristic bulkTransferControlPoint;
+
     RequestCallback    requestData;
 };
 
